530732cb8e07ed4797757aa92b256cc7de333274
CyberObiOne <hiva.vlad@gmail.com>
2021-07-30 13:20:13 +0300

    Update akash_commission.sh

--- akash/akash_commission.sh
+++ akash/akash_commission.sh
@@ -4,9 +4,9 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 DELEGATOR='Delegator_address' # your address created or imported
 VALIDATOR='Validator_address' # can get from Keplr (https://wallet.keplr.app/#/akashnet/stake?modal=detail&validator=akashvaloper14kn0kk33szpwus9nh8n87fjel8djx0y0uzn073) Forbole, as example
-PASWD='Your_pass' # from 22 line of ReadMe file
-DELAY=57600 #in secs # it depends of your stake
 ACC_NAME='Your_account_name' #Account name from Keyring
+PASWD='Your_pass' # from 22 line of ReadMe file
+DELAY=57600 #in secs # it depends on your stake
 for ((; ;));
  do
         BAL=$(./akash query bank balances ${DELEGATOR}  -o json | jq -r '.balances[].amount'); #current balance
@@ -22,7 +22,7 @@ for ((; ;));
         BAL=$(./akash query bank balances ${DELEGATOR} -o json | jq -r '.balances[].amount'); # balance after claim
         echo -e "BALANCE: ${GREEN}${BAL}${NC}uakt\n"
         echo -e "Stake ALL\n"
-        COMMISSION=$((${BAL} - 500)) # commission fees
+        COMMISSION=$((${BAL} - 500)) # balance minus commission fees for paying transaction fees
         echo -e "${PASWD}\n" | ./akash tx staking delegate ${VALIDATOR} ${COMMISSION}uakt --from ${ACC_NAME} --fees 300uakt -y #delegate claim rewards
         for (( timer=${DELAY}; timer>0; timer-- ))
         do
